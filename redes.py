bce2b0e89c57a985d61e5e2f38975cfdd8b5fe49
joacoguerrae <joacoguerrae@gmail.com>
2025-07-01 08:01:27 -0300

    pruebas + redes + emprolijar notebook

--- redes.py
+++ redes.py
@@ -17,3 +17,25 @@ class FaceClassifierNN(nn.Module):
 
     def forward(self, x):
         return self.network(x)
+
+
+class FaceClassifier2NN(nn.Module):
+    def __init__(self, input_dim=459, hidden_dims=[512, 256, 128], output_dim=1):
+        super(FaceClassifier2NN, self).__init__()
+
+        layers = []
+        in_dim = input_dim
+
+        for h_dim in hidden_dims:
+            layers.append(nn.Linear(in_dim, h_dim))
+            layers.append(nn.BatchNorm1d(h_dim))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(0.3))
+            in_dim = h_dim
+
+        layers.append(nn.Linear(in_dim, output_dim))
+
+        self.network = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.network(x)
